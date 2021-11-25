f2c1880ef56653a1df3b2945cdc0552f31af9f07
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-11-26 00:00:19 +0600

    Start with heroku

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require('cors'); // Cross Origin Resource Sharing
+const compression = require('compression');
 const userRouter = require('./routers/userRouter');
 const orderRouter = require('./routers/orderRouter');
 
 const app = express();
 
+app.use(compression());
 app.use(cors());
 app.use(express.json());
 
